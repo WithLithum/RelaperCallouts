a281a2eb61b66dd963ae44f9102034894292fb64
RelaperCrystal <37800788+RelaperCrystal@users.noreply.github.com>
2021-03-29 21:10:47 +0800

    :bug: Fix unregistered callouts

--- src/RelaperCallouts/RelaperCallouts/Main.cs
+++ src/RelaperCallouts/RelaperCallouts/Main.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LSPD_First_Response.Mod.API;
+using Rage;
+using RelaperCallouts.Callouts;
 
 namespace RelaperCallouts
 {
@@ -15,7 +17,11 @@ namespace RelaperCallouts
 
         public override void Initialize()
         {
-            
+            Game.LogTrivial("Rel.C: Registering callouts...");
+            Functions.RegisterCallout(typeof(ArmoredCarRobbery));
+
+            Game.DisplayNotification("web_lossantospolicedept", "web_lossantospolicedept", "RelaperCallouts", "~y~by RelaperCrystal", "~b~has been loaded. ~g~Enjoy!");
+            Game.LogTrivial("Rel.C: End initializing RelaperCallouts");
         }
     }
 }
